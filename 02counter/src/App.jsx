6c63d78a50c140449bb8348beb2a5d6d235fe2e9
uyraju7 <uppulayesuraju7@gmail.com>
2024-06-21 17:14:47 +0530

    chore: counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import "./App.css"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+   const addValue = () => {
+    setCount((count) => count + 1)
+    setCount((count) => count + 1)
+  
+  } ;
+
+    const removeValue = () => {
+      setCount((count) => count - 1)} ;
+
+  return (
+
+
+    <div>
+     
+    
+     <h1> hi this my react code {count}</h1>
+
+
+        <button onClick={addValue}>    ADD     </button> {" "}
+          
+        <button onClick={removeValue}>    Remove     </button>
+
+        
+     
+       
+    </div>
+  )
+}
+
+export default App
